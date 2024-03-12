bccefd73a67c09ad6060d140198638d4084dab10
SPRINGPEACHVINH <daoxuan.vinh3@gmail.com>
2024-03-13 04:29:02 +0700

    UI calculator

---
+++ TaskW1-2/Form1.cs
@@ -0,0 +1,37 @@
+namespace TaskW1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
